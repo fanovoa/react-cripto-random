056f83c73ea11f6db261e48e4e440932b6afec3b
fanovoa <fabian93novoa@gmail.com>
2023-08-10 17:17:56 -0500

    se incluye el README.md

---
+++ README.md
@@ -0,0 +1,18 @@
+# React + TypeScript + Vite
+
+Aplicación de React que trae un numero aleatorio - inicio del ueso de @tanstack/react-query 
+
+## Project setup
+```
+yarn i
+```
+
+### Compiles and hot-reloads for development
+```
+yarn dev
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import './App.css';
+import { useRandom } from './hooks/useRandom';
+
+
+export const App =() =>  {
+
+  const query = useRandom();
+
+  return (
+    <div className="App App-header">
+      {
+        query.isFetching 
+        ? ( <h2>Cargando...</h2>   )
+        : ( <h2>Número aleatorio: {query.data}</h2>)
+      }
+
+      {
+        !query.isLoading && query.isError && ( <h3>{ `${query.error}` }</h3>)
+      }
+      
+      <button onClick={ () => query.refetch() } disabled={query.isFetching }>
+        {
+          query.isFetching ? '...' : 'Nuevo número'
+        }
+      </button>
+    </div>
+  );
+}
+
+
